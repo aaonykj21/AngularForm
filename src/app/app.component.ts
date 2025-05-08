2c4086446fd5b6f166f2e8324f5e002aff09ebc6
aaonykj21 <aon.natcha29@gmail.com>
2025-05-08 09:31:47 +0700

    connect to LineLiff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import liff from '@line/liff';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'coop';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    liff.init({ liffId: '2007376637-MwJPe7Lw' }) 
+      .then(() => {
+        if (!liff.isLoggedIn()) {
+          liff.login(); 
+        } else {
+          console.log('User is logged in');
+        }
+      })
+      .catch((err) => {
+        console.error('LIFF init error', err);
+      });
+  }
 }
